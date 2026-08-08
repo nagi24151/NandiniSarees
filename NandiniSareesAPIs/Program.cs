42ea55153072c59cdd4742d44bcaad9e9499b3b4
nagis <nagisetty1@gmail.com>
2026-08-08 12:56:05 +0530

    added default APIs

---
+++ NandiniSareesAPIs/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Map root to an existing endpoint so requests to '/' don't return 404.
+app.MapGet("/", () => Results.Redirect("/weatherforecast"));
+
+app.Run();
